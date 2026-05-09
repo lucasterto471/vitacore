e4f49e64d06f8759dfabf6561844b0ab931046ae
lucasterto471 <lucasterto471@gmail.com>
2026-05-09 14:27:41 -0300

    feat: remover slide de instalacao do onboarding

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="vitacore-v5",ASSETS=["./","index.html","css/main.css","js/storage.js?v=5","js/charts.js?v=5","js/pages.js?v=5","js/exercises.js?v=5","js/app.js?v=5","manifest.json"];
+const CACHE="vitacore-v6",ASSETS=["./","index.html","css/main.css","js/storage.js?v=6","js/charts.js?v=6","js/pages.js?v=6","js/exercises.js?v=6","js/app.js?v=6","manifest.json"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).then(r=>{if(r&&r.status===200){const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c))}return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("index.html"))))});
